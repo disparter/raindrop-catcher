b76d69a7805a310e6346c719d5d546d82a91031b
Felipe Faria <felipe.faria@waitrose.co.uk>
2023-04-19 16:25:24 -0300

    Adding new launchers

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.badlogic.drop.Drop;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new MyGdxGame(), config);
+		new Lwjgl3Application(new Drop(), config);
 	}
 }
