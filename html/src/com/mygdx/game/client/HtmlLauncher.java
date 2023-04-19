cbe2254b4f2b168e30bef0db6d1ee6cc44aeccb4
Felipe Faria <felipe.faria@waitrose.co.uk>
2023-04-19 16:35:53 -0300

    Fixed import

--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.drop.Drop;
 
 public class HtmlLauncher extends GwtApplication {
         @Override
