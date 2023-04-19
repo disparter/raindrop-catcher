873fa3962696837f36210bed2f94299a24589ce1
Felipe Faria <felipe.a.b.faria@gmail.com>
2023-04-19 20:00:42 -0300

    Adding keydown on show method

--- core/src/com/mygdx/drop/screens/GameScreen.java
+++ core/src/com/mygdx/drop/screens/GameScreen.java
@@ -1,7 +1,9 @@
 package com.mygdx.drop.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -20,7 +22,9 @@ import com.mygdx.drop.entities.RaindropPool;
 
 import java.util.Iterator;
 
-public class GameScreen implements Screen {
+import static com.mygdx.drop.entities.Constants.BUCKET_MOVEMENT;
+
+public class GameScreen extends ScreenAdapter {
 	final DropGame game;
 	private Sound dropSound;
 	private Sound rainMusic;
@@ -65,7 +69,23 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void show() {
+		Gdx.input.setInputProcessor(new InputAdapter() {
+			@Override
+			public boolean keyDown(int keycode) {
+				if (keycode == Input.Keys.LEFT) {
+					bucket.setPosition(bucket.getPosition().x - BUCKET_MOVEMENT * Gdx.graphics.getDeltaTime(), bucket.getPosition().y);
+				}
+				if (keycode == Input.Keys.RIGHT) {
+					bucket.setPosition(bucket.getPosition().x + BUCKET_MOVEMENT * Gdx.graphics.getDeltaTime(), bucket.getPosition().y);
+				}
+				return true;
+			}
 
+			@Override
+			public boolean keyUp(int keycode) {
+				return true;
+			}
+		});
 	}
 
 	@Override
