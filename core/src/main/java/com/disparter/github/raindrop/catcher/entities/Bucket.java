fa2781731d9f6bc12b2abcd7cc6493c077a928ec
Felipe Faria <felipe.a.b.faria@gmail.com>
2023-04-21 11:43:15 -0300

    Fixing html module

--- core/src/main/java/com/disparter/github/raindrop/catcher/entities/Bucket.java
+++ core/src/main/java/com/disparter/github/raindrop/catcher/entities/Bucket.java
@@ -5,13 +5,16 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 
+import static com.disparter.github.raindrop.catcher.entities.Constants.BUCKET_HEIGHT;
+import static com.disparter.github.raindrop.catcher.entities.Constants.BUCKET_WIDTH;
+
 public class Bucket {
     private final Sprite sprite;
     private final Rectangle bounds;
 
     public Bucket() {
         sprite = new Sprite(new Texture(Gdx.files.internal("bucket.png")));
-        bounds = new Rectangle(320 / 2 - 64 / 2, 20, Constants.BUCKET_WIDTH, Constants.BUCKET_HEIGHT);
+        bounds = new Rectangle(320 / 2 - 64 / 2, 20, BUCKET_WIDTH, BUCKET_HEIGHT);
     }
 
     public Rectangle getBounds() {
