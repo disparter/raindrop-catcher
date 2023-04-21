17c97474fc10d1d6ebfebd6909e04e9c6ff18108
Felipe Faria <felipe.a.b.faria@gmail.com>
2023-04-21 11:44:05 -0300

    Adding import

--- html/src/main/java/com/github/disparter/raindrop/catcher/html/client/HtmlLauncher.java
+++ html/src/main/java/com/github/disparter/raindrop/catcher/html/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.github.disparter.raindrop.catcher.html.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.disparter.raindrop.catcher.core.DropGame;
 
 
 public class HtmlLauncher extends GwtApplication {
